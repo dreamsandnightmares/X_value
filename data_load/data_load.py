0c4af3f911a35de4aca7d2bfe37822407dcc24ec
wch <905080303@qq.com>
2023-02-20 22:57:28 +0800

    x_gen_switch——polt

--- data_load/data_load.py
+++ data_load/data_load.py
@@ -42,55 +42,57 @@ def data_load():
 
 if __name__ == '__main__':
     pd_load,pd_price,pd_wea_wind,pd_wea_G_dir,pd_wea_G_diff,pd_wea_T ,pd_wea_G_hor= data_load()
-    pd_price = pd_price[:504]
+
     dist_price = list(range(len(pd_price)))
+    plt.plot(dist_price,pd_price)
 
 
-    fig,([axs3,axs4],[axs5,axs6]) = plt.subplots(2,2)
 
-    # axs1.plot(dist_price,pd_price)
-    # axs1.set_xlabel('Time [h]')
-    # axs1.set_ylabel('Price [$/kwh]')
-    # axs1.set_title('Power_price ')
-    # #
-    # #
-    # #
-    # #
-    # pd_load =pd_load[:504]
-    # dist_load = list(range(len(pd_load)))
-    # axs2.plot(dist_load,pd_load)
-    # axs2.set_xlabel('Time [h]')
-    # axs2.set_ylabel('Load [kwh]')
-    # axs2.set_title('Power_load ')
-
-    dist_G_dir = list(range(len(pd_wea_G_dir)))
-    axs3.plot(dist_G_dir,pd_wea_G_dir)
-    axs3.set_xlabel('Time [h]')
-    axs3.set_ylabel('solar irradiance dir [W/m²]')
-    axs3.set_title('solar irradiance dir ')
-
-    dist_G_dif = list(range(len(pd_wea_G_diff)))
-    axs4.plot(dist_G_dif,pd_wea_G_diff)
-    axs4.set_xlabel('Time [h]')
-    axs4.set_ylabel('solar irradiance diff [W/m²]')
-    axs4.set_title('solar irradiance diff ')
-
-
-    dist_wind = list(range(len(pd_wea_wind)))
-    axs5.plot(dist_wind,pd_wea_wind)
-    axs5.set_xlabel('Time [h]')
-    axs5.set_ylabel('wind speed [m/s]')
-    axs5.set_title('wind speed ')
-
-    dist_G_hor = list(range(len(pd_wea_G_hor)))
-    axs6.plot(dist_G_hor,pd_wea_G_hor)
-    axs6.set_xlabel('Time [h]')
-    axs6.set_ylabel('solar irradiance hor [W/m²]')
-    axs6.set_title('solar irradiance hor ')
+    # fig,([axs3,axs4],[axs5,axs6]) = plt.subplots(2,2)
+    #
+    # # axs1.plot(dist_price,pd_price)
+    # # axs1.set_xlabel('Time [h]')
+    # # axs1.set_ylabel('Price [$/kwh]')
+    # # axs1.set_title('Power_price ')
+    # # #
+    # # #
+    # # #
+    # # #
+    # # pd_load =pd_load[:504]
+    # # dist_load = list(range(len(pd_load)))
+    # # axs2.plot(dist_load,pd_load)
+    # # axs2.set_xlabel('Time [h]')
+    # # axs2.set_ylabel('Load [kwh]')
+    # # axs2.set_title('Power_load ')
+    #
+    # dist_G_dir = list(range(len(pd_wea_G_dir)))
+    # axs3.plot(dist_G_dir,pd_wea_G_dir)
+    # axs3.set_xlabel('Time [h]')
+    # axs3.set_ylabel('solar irradiance dir [W/m²]')
+    # axs3.set_title('solar irradiance dir ')
+    #
+    # dist_G_dif = list(range(len(pd_wea_G_diff)))
+    # axs4.plot(dist_G_dif,pd_wea_G_diff)
+    # axs4.set_xlabel('Time [h]')
+    # axs4.set_ylabel('solar irradiance diff [W/m²]')
+    # axs4.set_title('solar irradiance diff ')
     #
     #
-    fig.subplots_adjust(wspace=0.5,hspace=0.5)
-    plt.savefig('wea_data.svg', format='svg')
+    # dist_wind = list(range(len(pd_wea_wind)))
+    # axs5.plot(dist_wind,pd_wea_wind)
+    # axs5.set_xlabel('Time [h]')
+    # axs5.set_ylabel('wind speed [m/s]')
+    # axs5.set_title('wind speed ')
+    #
+    # dist_G_hor = list(range(len(pd_wea_G_hor)))
+    # axs6.plot(dist_G_hor,pd_wea_G_hor)
+    # axs6.set_xlabel('Time [h]')
+    # axs6.set_ylabel('solar irradiance hor [W/m²]')
+    # axs6.set_title('solar irradiance hor ')
+    # #
+    # #
+    # fig.subplots_adjust(wspace=0.5,hspace=0.5)
+    plt.savefig('load_data.svg', format='svg')
 
     plt.show()
 
