16d7434c901dfe05223d8e602d03a2f359785d0a
wch <905080303@qq.com>
2023-02-20 20:12:41 +0800

    x_gen_switch——polt

--- main.py
+++ main.py
@@ -89,62 +89,95 @@ def max_R(time_range:int,n,E_max,h,eff,C_gen,C_power,C_storage):
 
 
 if __name__ == '__main__':
-    # time_range =490
-    # pd_load, pd_price, pd_wea_wind, pd_wea_G_dir, pd_wea_G_diff, pd_wea_T, pd_wea_G_hor = clip(time_range)
-    # x_gen  =X_gen(pv_power_rate=220,time_load=time_range)
-    #
-    # load_nor = load_nor(pd_load)
-    # n = 490
-    # eff = 0.9
-    #
-    #
-    #
-    # x,y=solver(x_gen[:490],pd_price,E_max=1,eff=0.9,h=1)
+    time_range =490
+    pd_load, pd_price, pd_wea_wind, pd_wea_G_dir, pd_wea_G_diff, pd_wea_T, pd_wea_G_hor = clip(time_range)
+    x_gen  =X_gen(pv_power_rate=220,time_load=time_range)
+
+    load_nor = load_nor(pd_load)
+    n = 490
+    eff = 0.9
+
+
+
+    x,y=solver(x_gen[:490],pd_price,E_max=1,eff=0.9,h=1,n=n)
     # print(x,y)
-    # draw_data_plt(False,x_gen,pd_price,load_nor,pd_load)
-    #
-    # Revenue_Gen =Revenue_gen(x_gen,pd_price)
-    # R_tot= Revenue_all(Revenue_Gen,x,y,n,eff,pd_price)
+    draw_data_plt(False,x_gen,pd_price,load_nor,pd_load)
+
+    Revenue_Gen =Revenue_gen(x_gen,pd_price)
+    R_tot= Revenue_all(Revenue_Gen,x,y,n,eff,pd_price)
     # print(R_tot,'R_tot')
-    # crf = CRF()
-    # X = x_value(R_tot,crf,C_gen=1000,C_power=50,C_storage=50,E_max=3,h=2)
-    # x_max = max_R(time_range=8000,n=456,E_max=1,h=1,eff=0.9,C_gen=1000,C_power=100,C_storage=100)
+    crf = CRF()
+    X = x_value(R_tot,crf,C_gen=1000,C_power=50,C_storage=50,E_max=3,h=2)
+    x_max = max_R(time_range=8000,n=456,E_max=1,h=1,eff=0.9,C_gen=1000,C_power=100,C_storage=100)
     # print(x_max)
+    dist_x_gen  =list(range(len(x_gen)))
+
+
+    #
+    x_gen_new = []
+
+    for i in range(len(x_gen)):
+        x_gen_new.append(x_gen[i]+x[i]-y[i])
+
+    dist_x_gen_new =list(range(len(x_gen_new)))
+    fig, (axs1, axs2,axs3) = plt.subplots(3, 1,sharex=True)
+    axs1.plot(dist_x_gen_new, x_gen_new)
+    axs1.set_xlabel('Time')
+    axs1.set_ylabel('X_gen_new ')
+    axs1.set_title('X_gen_new ')
+
+    axs2.plot(dist_x_gen, x_gen)
+    axs2.set_xlabel('Time')
+    axs2.set_ylabel('X_gen ')
+    axs2.set_title('X_gen ')
+
+    axs3.plot(dist_x_gen_new, pd_price)
+    axs3.set_xlabel('Time')
+    axs3.set_ylabel('price ')
+    axs3.set_title('price')
+    fig.subplots_adjust(wspace=0.5, hspace=0.5)
+    plt.savefig('X_gen switch.svg', format='svg')
+
+    plt.show()
 
 
 
-    C_power = [1190, 952, 590]
-    C_storage = [93, 74 , 46]
-    E_max_ra = np.arange(0.1, 4,0.5)
-    print(E_max_ra)
-    h_ra = np.arange(0.1, 4,0.25 )
-    for power in C_power:
-        for storage in C_storage:
-            Z = []
-            for i in range(len(E_max_ra)):
-                Z_X = []
-                print(E_max_ra[i],'E_max')
-                for j in range(len(h_ra)):
-                    print(h_ra[j],'h')
-                    x_max = max_R(time_range=8000, n=456, E_max=i, h=j, eff=0.9, C_gen=1000, C_power=power, C_storage=storage)
-
-                    Z_X.append(x_max)
-                Z.append(Z_X)
-            Z = np.array(Z)
-            z = Z.tolist()
-
-            ctf = plt.contourf(h_ra,E_max_ra,  z, 60, cmap='RdGy')
 
 
-            plt.colorbar()  # 添加cbar
-            plt.title('storage{}! power{}!'.format(storage,power))
-            plt.xlabel(('storage time'))  # 去掉x标签
-            plt.ylabel(('storage ratio'))  # 去掉y标签
 
-            plt.savefig('25storage{}+power{}.svg'.format(storage,power),format='svg')
-            plt.clf()
-            print(Z)
 
+    # C_power = [1190, 952, 590]
+    # C_storage = [93, 74 , 46]
+    # E_max_ra = np.arange(0.1, 4,0.5)
+    # print(E_max_ra)
+    # h_ra = np.arange(0.1, 4,0.25 )
+    # for power in C_power:
+    #     for storage in C_storage:
+    #         Z = []
+    #         for i in range(len(E_max_ra)):
+    #             Z_X = []
+    #             print(E_max_ra[i],'E_max')
+    #             for j in range(len(h_ra)):
+    #                 print(h_ra[j],'h')
+    #                 x_max = max_R(time_range=8000, n=456, E_max=i, h=j, eff=0.9, C_gen=1000, C_power=power, C_storage=storage)
+    #
+    #                 Z_X.append(x_max)
+    #             Z.append(Z_X)
+    #         Z = np.array(Z)
+    #         z = Z.tolist()
+    #
+    #         ctf = plt.contourf(h_ra,E_max_ra,  z, 60, cmap='RdGy')
+    #
+    #
+    #         plt.colorbar()  # 添加cbar
+    #         plt.title('storage{}! power{}!'.format(storage,power))
+    #         plt.xlabel(('storage time'))  # 去掉x标签
+    #         plt.ylabel(('storage ratio'))  # 去掉y标签
+    #
+    #         plt.savefig('25storage{}+power{}.svg'.format(storage,power),format='svg')
+    #         plt.clf()
+    #         print(Z)
+    #
 
 
 
